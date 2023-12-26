60d258dbaf93a6bc28c133e2987ca440bb77244a
Saikot Paul <saikotpaul123@gmail.com>
2023-12-25 23:20:45 -0500

    sticky navbar

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <header className="mx-20 my-5 border-b border-gray-50">
+    <header className="mx-auto px-20 py-5 border-b w-full h-full border-gray-50 sticky top-0 z-50 bg-black">
       <div className="flex justify-between items-center">
         <div>
           <Link href="/home" className="text-3xl font-bold align-text-bottom">
             Saikot Paul
           </Link>
         </div>
-        <div className="flex space-x-4 text-lg align-bottom p-0">
+        <div className="flex space-x-4 text-lg">
           <Link href="home">Home</Link>
           <Link href="experience">Experience</Link>
           <Link href="projects">Projects</Link>
