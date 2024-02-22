aac115bab6cb25bfb4c4d87d8f93bf2e88d074df
Saikot Paul <saikotpaul123@gmail.com>
2024-02-22 18:18:53 -0500

    dummy push

--- components/main/AboutMe.tsx
+++ components/main/AboutMe.tsx
@@ -9,13 +9,13 @@ interface skill {
 const AboutMe = () => {
   return (
     <section id="about">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center items-center">
         <h1 className="text-3xl font-bold my-6">
           Who is Saikot?
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
         </h1>
-        <div className="flex flex-col md:flex-row justify-center align-top">
-          <div className="flex flex-col my-2 px-4 md:w-1/3 w-full">
+        <div className="flex flex-col justify-center items-center md:w-full">
+          <div className="flex flex-col my-2 px-4 w-full items-center">
             <h2 className="text-2xl font-bold mb-3 text-center">About me</h2>
             <p className="text-lg mb-2 text-wrap md:text-justify">
               My journey into the world of programming began with a spark of
--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -20,16 +20,16 @@ const NavBar = () => {
 
   return (
     <header className="px-20 py-5 border-b w-full h-full border-gray-50 sticky top-0 z-50 bg-black">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between space-x-2 items-center">
         <div>
           <a
-            className="text-3xl font-bold align-text-bottom"
+            className="text-2xl md:text-3xl font-bold align-text-bottom text-full"
             onClick={scrollToSection("home", defaultOffset)}
           >
             Saikot Paul
           </a>
         </div>
-        <div className="flex space-x-4 text-lg">
+        <div className="flex space-x-1 md:space-x-4 text-sm md:text-lg">
           <a href="#home" onClick={scrollToSection("home", defaultOffset)}>
             Home
           </a>
